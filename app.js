79e1fd406affbe6237a97468bb548dbfafeb69fe
unknown <remcomooij@gmail.com>
2020-03-09 16:51:03 -0400

    updated chart

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ function buildPlot() {
     d3.json("samples.json").then(function(samples) {
         var data = [{
             type: 'bar',
-            x: samples.sample_values,
-            y: samples.otu_ids,
+            x: samples.sample_values.map(row => row),
+            y: samples.otu_ids.map(row => row),
             orientation: 'h'
         }];
 
