9ec3c0c0ee7d037f0e1fb9e402b4e95573fb523f
unknown <remcomooij@gmail.com>
2020-03-09 13:52:01 -0400

    created graph

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,14 @@
 
 function buildPlot() {
-    d3.json("samples.json").then(function(data) {
-        var names = samples.names;
-        console.log(names);
+    d3.json("samples.json").then(function(samples) {
+        var data = [{
+            type: 'bar',
+            x: samples.sample_values,
+            y: samples.otu_ids,
+            orientation: 'h'
+        }];
 
+    Plotly.newPlot("bar", data);
 
     });
 };
