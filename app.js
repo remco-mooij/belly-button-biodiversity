e05c507d9fe524476da4de1c36c8f58815c0ae9f
unknown <remcomooij@gmail.com>
2020-03-10 18:41:32 -0400

    made changes to function:

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 
-function buildPlot() {
-    d3.json("samples.json").then(function(samples) {
-        var data = [{
+function buildPlot(sample) {
+    d3.json("samples.json").then((data) => {
+        var samples = data.samples;
+        var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        var plot = [{
             type: 'bar',
-            x: samples.sample_values.map(row => row),
-            y: samples.otu_ids.map(row => row),
+            x: otu_labels,
+            y: otu_ids,
             orientation: 'h'
         }];
 
-    Plotly.newPlot("bar", data);
+    Plotly.newPlot("bar", plot);
 
     });
 };
