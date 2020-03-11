e75d5981a0eab7df649bdd372a9e1522c2fbb259
unknown <remcomooij@gmail.com>
2020-03-11 15:41:28 -0400

    made some changes

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 
     d3.json("samples.json").then((importedData) => {
         var data = importedData;
+        var xValues = data.samples.map(row => row.otu_ids);
+        var yValues = data.samples.map(row => row.sample_values);
+
 
         var trace1 = {
             type: 'bar',
-            x: data.samples.map(row => row.otu_ids),
-            y: data.samples.map(row => row.sample_values),
+            x: xValues,
+            y: yValues,
             orientation: 'h'
         };
 
         var chartData = [trace1];
+
+        var layout = {
+            xaxis: {
+                range: [xValues]
+            }
+        };
 
     Plotly.newPlot("bar", chartData);
 
