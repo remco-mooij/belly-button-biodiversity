24cdb14ac70fb48a3f739995d3927c18b5574402
unknown <remcomooij@gmail.com>
2020-03-13 23:17:32 -0400

    Finished assignment

--- app.js
+++ app.js
@@ -1,28 +1,121 @@
 
+
+
+
+// Add ID numbers to dropdown menu and build initial dashboard
+function init() {
+    d3.json("samples.json").then((importedData) => {
+        var names = importedData.names;
+        var dropdownList = d3.select("#selDataset");
+
+        names.forEach(function(name) {
+            dropdownList.append("option")
+            .text(`${name}`);
+        });
+
+        // Call buildData() function to build initial dashboard
+        buildData();
+    });
+};
+
+init();
+
+// Call buildData() function when user select an ID number from dropdown menu
+d3.selectAll("#selDataset").on("change", buildData);
+
+function buildData() {
+    var selection = d3.select("#selDataset").property("value");
+    console.log(selection);
+
     d3.json("samples.json").then((importedData) => {
         var samples = importedData.samples;
-        var resultArray = samples.filter(sampleObject => sampleObject.id == importedData);
+        var resultArray = samples.filter(sampleObject => sampleObject.id == selection);
         var result = resultArray[0];
-        console.log(result);
-        // var xValues = data.map(row => row.samples.otu_ids);
-        // var yValues = data.map(row => row.samples.sample_values);
-        var xValues = result.otu_ids;
-        var yValues = result.sample_values;
-        console.log(xValues);
+          
+        var sampleValues = result.sample_values;
+        var otuIds = result.otu_ids;
+        var otuLabels = result.otu_labels;
+        
+        var xTopTen = sampleValues.slice(0, 10).reverse();
+        var yTopTen = otuIds.slice(0, 10).reverse();
+        var hoverTopTen = otuLabels.slice(0, 10).reverse();
 
-        var trace1 = {
+        console.log(xTopTen);
+        console.log(yTopTen);
+        console.log(hoverTopTen);
+
+        otuArray = [];
+        yTopTen.forEach(function(id) {
+            var otuName = `OTU ${id}`;
+            otuArray.push(otuName);
+        });
+        console.log(otuArray);
+
+        // Build charts
+        var barChart = [{
             type: 'bar',
-            x: xValues,
-            y: yValues,
-            orientation: 'h'
+            x: xTopTen,
+            y: otuArray,
+            orientation: 'h',
+            text: hoverTopTen
+        }];
+
+        var barLayout = {
+            width: 400,
+            height: 600,
+            xaxis: {
+                autorange: true,
+            },
+            hoverlabel: {
+                bgcolor: "#812305",
+                font: {color: "white"}
+            }
         };
 
-        var chartData = [trace1];
+        var bubbleChart = [{
+            x: otuIds,
+            y: sampleValues,
+            mode: 'markers',
+            text: otuLabels,
+            marker: {
+                size: sampleValues,
+                color: otuIds
+            }
+        }];
 
-//hello
+        var bubbleLayout = {
+            xaxis: {
+                title: "OTU ID"
+            }
+        };
 
-    Plotly.newPlot("bar", chartData);
+    Plotly.newPlot("bubble", bubbleChart, bubbleLayout);
+    Plotly.newPlot("bar", barChart, barLayout);
+
+    // Call displayMetadata() function to display demographic info 
+    displayMetadata();
 
     });
+};
 
-// buildPlot();   
+function displayMetadata() {
+    d3.json("samples.json").then((importedData) => {
+        var metaData = importedData.metadata
+        var selection = d3.select("#selDataset").property("value");
+    
+        d3.select("#sample-metadata").selectAll("p").remove();
+        var metaDataObject = metaData.filter(sampleObject => sampleObject.id == selection);
+        console.log(metaDataObject);
+        var metaDataValues = metaDataObject[0];
+    
+        Object.entries(metaDataValues).forEach(([key, value]) => {
+            console.log(`${key}: ${value}`);
+            d3.select("#sample-metadata")
+            .append("p")
+            .text(`${key}: ${value}`);
+        }); 
+    });
+};
+    
+   
+    
